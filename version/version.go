78784ea08261585d07374a1945caf47122a99a98
0xmattg <0xmattg@gmail.com>
2026-05-08 21:04:28 +0800

    chore: remove beta label

--- version/version.go
+++ version/version.go
@@ -3,10 +3,10 @@ package version
 import "fmt"
 
 const (
-	Major = 0        // Major version component of the current release
-	Minor = 6        // Minor version component of the current release
-	Patch = 1        // Patch version component of the current release
-	Meta  = "beta.1" // Version metadata to append to the version string
+	Major = 0  // Major version component of the current release
+	Minor = 6  // Minor version component of the current release
+	Patch = 1  // Patch version component of the current release
+	Meta  = "" // Version metadata to append to the version string
 )
 
 func String() string {
